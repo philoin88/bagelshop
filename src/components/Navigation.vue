67eae7c0b654fc37ce1a4368c4bf717161991f7e
Robin <philoin88@gmail.com>
2019-09-15 19:00:55 +0900

    프로젝트 기본 세팅

---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
